e4b4e9f130bc18b9e09ac7ee9a31e995f75107f9
Bayram <bxojametov97@gmail.com>
2025-08-22 15:31:29 +0500

    first commit

--- src/Router.jsx
+++ src/Router.jsx
@@ -5,7 +5,8 @@ import { Auth } from "./pages/Auth";
 import { Home } from "./pages/Home";
 import { Category } from "./pages/Category";
 import { Words } from "./pages/Words";
-import { AddWords } from "./pages/Words/addWords";
+import { AddWords } from "./pages/Words/AddWords";
+
 import { EditWords } from "./services/words/EditWords";
 
 export const Router = () => {
@@ -13,7 +14,7 @@ export const Router = () => {
     <Routes>
       <Route path="/" element={<AdminLayout />}>
         <Route index element={<Home />} />
-        <Route path="/category" element={<Category />} />
+        <Route path="/category" element={<Category />} />=
         <Route path="/words" element={<Words />} />
         <Route path="/words/addwords" element={<AddWords />} />
         <Route path="/words/editwords/:id" element={<EditWords />} />
--- src/pages/Words/AddWords.jsx
+++ src/pages/Words/AddWords.jsx
@@ -1,7 +1,6 @@
 import { Button } from "antd";
+import { useNavigate } from "react-router";
 import { WordsForm } from "../../components/words-form/WordsForm";
-import { PlusOutlined } from "@ant-design/icons";
-import { Navigate, useNavigate } from "react-router";
 
 export const AddWords = () => {
   const navigate = useNavigate();
